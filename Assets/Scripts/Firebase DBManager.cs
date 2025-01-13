dac92ff5876143f360192b50e550f5064afab6ff
bestgo1228 <bestgo1228@naver.com>
2025-01-13 18:51:21 +0900

    FIX_준수 최종 수정

--- Assets/Scripts/Firebase DBManager.cs
+++ Assets/Scripts/Firebase DBManager.cs
@@ -1,11 +1,19 @@
+using Firebase;
+using System;
 using UnityEngine;
+using Firebase.Database;
 
 public class FirebaseDBManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] string dbURL;
+
+    bool isReceived = false;
+
     void Start()
     {
-        
+        FirebaseApp.DefaultInstance.Options.DatabaseUrl = new System.Uri(dbURL);
+
     }
 
     // Update is called once per frame
