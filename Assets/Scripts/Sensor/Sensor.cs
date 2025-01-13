45c659fad0a300599540f57763fda2c76e415aaa
조연하 <dusgk629@gmail.com>
2025-01-13 17:34:43 +0900

    FEAT:250113_UNITY
    - Sensor 수정
    - Gate 함수 수정

--- Assets/Scripts/Sensor/Sensor.cs
+++ Assets/Scripts/Sensor/Sensor.cs
@@ -15,6 +15,7 @@ public class Sensor : MonoBehaviour
     public void Start()
     {
         //OnSensorTower("green");
+        OnVacuumSensor(0);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
